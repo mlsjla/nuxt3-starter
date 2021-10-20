4b45d42976885616be361338b0cb90f3655714f4
杜洪浪 <duhonglang@cheesestar.com>
2021-10-20 11:35:21 +0800

    first

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- Remove this component to get started! -->
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    onMounted(() => {
+      // I want to get process.env here ...
+      console.log('I cat get process.env here ...')
+      // I can get process.env. But I don't know how to add variables in process.env
+      console.log(process.env)
+      // Like this
+      console.log(process.env.variables)
+
+      /*
+       *  But I want to add other variables in process.env
+       *  Like process.env.Base_URL or process.env.TIMEOUT etc..
+       *  Because I have some sdks require process.env.variables
+       *  
+       *  If I use process.env with 'const config = useRuntimeConfig() ' , I must edit my sdks code
+       *  But I can't
+       */
+
+    })
+  },
+}
+</script>
